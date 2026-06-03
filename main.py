85c870ea6b30e75bb27b43a9814753b550f873cc
Lekhanath  Bhattarai <lekhanathb0@gmail.com>
2026-06-03 15:27:04 +0545

    Create initial flask GUI for chainproof

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Chainproof installed sucessfully.")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+@app.route("/verify")
+def verify():
+    return render_template("verify.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
